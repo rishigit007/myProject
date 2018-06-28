37b2c8771374a1ff01fb7dfc4289d6b3aaa794ef
HP1 <indranil.goswami14@gmail.com>
2018-06-28 17:47:19 +0530

    Fisrt commit for SpringRest Project

---
+++ src/main/java/com/learning/git/SpringRest/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.learning.git.SpringRest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/hello")
+	public String helloWorld()
+	{
+		return "Hello-world";
+	}
+
+}
